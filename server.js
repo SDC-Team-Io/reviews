f00241cd5d05256622ec095c934e1e524a882936
LukeLarson2 <lucas.m.larson2@gmail.com>
2023-12-07 10:57:54 -0800

    updated tables

--- server.js
+++ server.js
@@ -17,16 +17,75 @@ const pool = new Pool({
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './public/dist')));
 
-app.get('/characteristics', async (req, res) => {
+app.get('/reviews/meta', async (req, res) => {
   try {
-    const queryText = "SELECT char_reviews.characteristic_id, value, name FROM char_reviews INNER JOIN (SELECT * FROM characteristics WHERE product_id = 5) AS char_query ON char_reviews.characteristic_id = char_query.characteristic_id;";
-    const response = await pool.query(queryText);
-    res.json(response.rows);
+    const productId = 40344;
+
+    // Ratings aggregation
+    const ratingsQuery = `
+      SELECT rating, COUNT(*) as count
+      FROM reviews
+      WHERE product_id = $1
+      GROUP BY rating;
+    `;
+
+    // Recommendations aggregation
+    const recommendationsQuery = `
+      SELECT recommend, COUNT(*) as count
+      FROM reviews
+      WHERE product_id = $1
+      GROUP BY recommend;
+    `;
+
+    // Characteristics aggregation
+    const characteristicsQuery = `
+      SELECT c.name, cr.characteristic_id, AVG(cr.value) as average_value
+      FROM characteristics c
+      INNER JOIN char_reviews cr ON c.characteristic_id = cr.characteristic_id
+      WHERE c.product_id = $1
+      GROUP BY c.name, cr.characteristic_id;
+    `;
+
+    // Execute queries
+    const ratingsResult = await pool.query(ratingsQuery, [productId]);
+    const recommendationsResult = await pool.query(recommendationsQuery, [productId]);
+    const characteristicsResult = await pool.query(characteristicsQuery, [productId]);
+
+    // Process results
+    const ratings = ratingsResult.rows.reduce((acc, row) => {
+      acc[row.rating] = parseInt(row.count, 10);
+      return acc;
+    }, {});
+
+    const recommended = recommendationsResult.rows.reduce((acc, row) => {
+      acc[row.recommend] = parseInt(row.count, 10);
+      return acc;
+    }, {});
+
+    const characteristics = characteristicsResult.rows.reduce((acc, row) => {
+      acc[row.name] = {
+        id: row.characteristic_id,
+        value: parseFloat(row.average_value).toFixed(2)
+      };
+      return acc;
+    }, {});
+
+    // Combine into final result
+    const response = {
+      product_id: productId,
+      ratings,
+      recommended,
+      characteristics
+    };
+
+    res.json(response);
+
   } catch (error) {
     console.error(error);
     res.status(500).send('Server Error');
   }
 });
+
 app.get('/reviews', async (req, res) => {
   const id = "40344"
   try {
